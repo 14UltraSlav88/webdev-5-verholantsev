c5fc5f1828cefa3691ffd1a3bce69d7c895ebfe5
UltraSlav <pashaverholantsev@gmail.com>
2025-10-28 05:53:51 +0000

    123

--- omdb/src/components/MovieCard/MovieCard.jsx
+++ omdb/src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,16 @@
-const MovieCard = () => {
+import { useNavigate } from 'react-router'
+import './MovieCard.css'
+
+const MovieCard = ({Title, Year, imdbId, Poster}) => {
+    const navigate = useNavigate()
+    const handleClick = () => {
+        navigate('/movie/${imdbId}')
+    }
     return (
-        <div className="movie-card">
-            <h4 className="movie-card__title"></h4>
-            <img src="" alt="" className="movie-card__image" />
-            <div className="movie-card__year"></div>
+        <div onClick={handleClick} className="movie-card">
+            <h4 className="movie-card__title">{Title}</h4>
+            <img src={Poster} alt="" className="movie-card__image" />
+            <div className="movie-card__year">{Year}</div>
         </div>
     )
 }
